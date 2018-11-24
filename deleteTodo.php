89966e8a8947cb63628f6f55799f8f0a84013196
heytehm <44373701+heytehm@users.noreply.github.com>
2018-11-24 19:25:56 +0100

    Delete deleteTodo.php

--- deleteTodo.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_GET['titre'])) {
-    if (isset($_SESSION['listeNotes'][$_GET['titre']])) {
-        unset($_SESSION['listeNotes'][$_GET['titre']]);
-    }
-}
-
-header('location:noteSession.php');
